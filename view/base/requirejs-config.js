7fcab34d3be4cb66f22d8d16219ef5b727afc7f5
Ronan <ronan.guerin@easyparapharmacie.com>
2022-06-27 13:42:43 +0200

    First commit

---
+++ view/base/requirejs-config.js
@@ -0,0 +1,6 @@
+var config = {
+    paths: {
+        'ui/template/form/element/multiselect': 'Ronangr1_EnhancedMultiSelect/template/form/element/multiselect',
+        'Magento_Ui/js/form/element/multiselect': 'Ronangr1_EnhancedMultiSelect/js/form/element/multiselect'
+    }
+};
